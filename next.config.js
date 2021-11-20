6f910ea15297afdf5fe52852fde49bb8cd09d84d
KissyWalk <36686279+KissyWalk@users.noreply.github.com>
2021-11-20 16:08:58 +0900

    fixed build error.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withOptimizedImages = require('next-optimized-images')
+
+module.exports = withOptimizedImages({
+    reactStrictMode: true,
+    handleImages: ['svg'],
+})
